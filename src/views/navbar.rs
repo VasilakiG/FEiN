0cda1a7dfdf008066a1e095008e1f3367be49970
Vasilaki Tocili <vasilakigorgi@gmail.com>
2025-09-04 23:59:31 +0200

    Move and stylize the navbar
    - Move it at the bottom of every screen
    - Stylized it
        - Made 4 links to navigate through the views
        - Added a circle with a plus sign in the middle for adding new transactions on the fly, floating at all times slightly above the navbar but part of it
        - Added CSS for all this and stylized it

--- src/views/navbar.rs
+++ src/views/navbar.rs
@@ -11,22 +11,22 @@ const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
 #[component]
 pub fn Navbar() -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: NAVBAR_CSS }
-
-        div {
-            id: "navbar",
-            Link {
-                to: Route::Home {},
-                "Home"
-            }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
-        }
-
         // The `Outlet` component is used to render the next component inside the layout. In this case, it will render either
         // the [`Home`] or [`Blog`] component depending on the current route.
         Outlet::<Route> {}
+
+        document::Link { rel: "stylesheet", href: NAVBAR_CSS }
+
+        div { id: "navbar",
+            Link { id: "navbar-item", to: Route::Home {}, "Home" }
+            Link { id: "navbar-item", to: Route::Blog { id: 1 }, "Blog" }
+
+            div { class: "fab-container",
+                button { class: "fab", "+" }
+            }
+
+            Link { id: "navbar-item", to: Route::Home {}, "Home" }
+            Link { id: "navbar-item", to: Route::Blog { id: 1 }, "Blog" }
+        }
     }
 }
