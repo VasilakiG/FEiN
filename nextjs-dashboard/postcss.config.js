25b259a8eeec97ccda1d2599e8c24e0379e2bfd7
Vasilaki Tocili <vasilakigorgi@gmail.com>
2026-01-21 00:21:23 +0100

    feature: Finish 13, 14 and 15 Chapter(Full Course NextJS App Router)

--- nextjs-dashboard/postcss.config.js
+++ nextjs-dashboard/postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
+    // autoprefixer is no longer needed in v4, as it's included by default with LightningCSS
   },
 };
