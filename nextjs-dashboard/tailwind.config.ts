87b89fc3a2a1d8cc49b096b9fe55dc4f1c0ba750
Vasilaki Tocili <vasilakigorgi@gmail.com>
2026-01-26 01:57:35 +0100

    feature: Add FEiN's design colours to the config
    - Remove the course-left stuff in the tailwind config, and replace them
    with the FEiN primary, primaryDark and accent colours, potentially
    upgrading them in the future, but keeping them in a single place for
    consistent design all over the UI

--- nextjs-dashboard/tailwind.config.ts
+++ nextjs-dashboard/tailwind.config.ts
@@ -8,14 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
+        fein: {
+          primary: '#249e86',
+          primaryDark: '#1d7f6a',
+          accent: '#1d7f6a',
         },
       },
     },
