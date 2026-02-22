e2234a0b0efed1e1cbf2a50b3ea86b8f5475f694
Vasilaki Tocili <vasilakigorgi@gmail.com>
2026-02-22 21:27:16 +0100

    chore: clean up unused file `lib/data.ts`

--- app/lib/data.ts
+++
@@ -1,6 +0,0 @@
-import postgres from 'postgres';
-import { } from './definitions';
-import { formatCurrency } from './utils';
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
